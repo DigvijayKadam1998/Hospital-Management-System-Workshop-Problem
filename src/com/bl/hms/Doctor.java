3321be8097f2142ee8ae4d13700ce3a613d29819
DigvijayKadam1998 <95114271+DigvijayKadam1998@users.noreply.github.com>
2021-12-16 23:32:51 +0530

    Update Doctor.java

--- src/com/bl/hms/Doctor.java
+++ src/com/bl/hms/Doctor.java
@@ -13,5 +13,18 @@ public class Doctor {
 	String specialisation;
 	String emailId;
 	long mobileNumber;
+	
 	Map<WeekDays, String> availability;
+	
+	@Override
+    public String toString() {
+        return "Doctor{" +
+                "docName='" + name + '\n' +
+                ", specialization='" + specialisation + '\n' +
+                ", emailID='" + emailId + '\n' +
+                ", id='" + id + '\n' +
+                ", mobNo=" + mobileNumber + '\n' +
+                ", availability=" + availability + '\n' +
+                '}';
+    }
 }
