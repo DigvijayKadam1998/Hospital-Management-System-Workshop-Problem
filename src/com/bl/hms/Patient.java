51cb66a485f1489d1281a2ba6186b9d9f8aeaa42
DigvijayKadam1998 <95114271+DigvijayKadam1998@users.noreply.github.com>
2021-12-16 23:34:02 +0530

    Update Patient.java

--- src/com/bl/hms/Patient.java
+++ src/com/bl/hms/Patient.java
@@ -1,5 +1,7 @@
 package com.bl.hms;
 
+import java.util.Map;
+
 public class Patient {
 	
 	enum Gender{
@@ -15,4 +17,20 @@ public class Patient {
 	String address;
 	String city;
 	String disease;
+	
+	Map<Gender,String> info;
+	
+	@Override
+    public String toString() {
+        return "Patient{" +
+                "name='" + name + '\n' +
+                ", address='" + address + '\n' +
+                ", emailID='" + emailId + '\n' +
+                ", disease='" + disease + '\n' +
+                ", patientId='" + id + '\n' +
+                ", mobNo=" + mobileNumber + '\n' +
+                ", age=" + age + '\n' +
+                ", info=" + info + '\n' +
+                '}';
+	}
 }
