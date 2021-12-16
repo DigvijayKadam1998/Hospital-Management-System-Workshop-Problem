7ad34a38dda31848a5577d62d822f405eefa1800
DigvijayKadam1998 <95114271+DigvijayKadam1998@users.noreply.github.com>
2021-12-16 23:36:12 +0530

    Update UserInterface.java

--- src/com/bl/hms/UserInterface.java
+++ src/com/bl/hms/UserInterface.java
@@ -1,16 +1,33 @@
 package com.bl.hms;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class UserInterface {
 	public int showMainMenu() {
-		System.out.println("Enter 1. Add Doctor \n + Enter 2. Delete Doctor \n 3. Update Doctor \n "
-				+ "4. Get Doctor \n 5. Get All Doctors \n Enter 6. Add Patient \\n + Enter 7. Delete Patient \\n 8. Update Patient \\n \"\r\n"
-				+ "9. Get Patient \n 10. Get All Patient \n Enter 11. Add Appointment \\n + Enter 12. Delete Appointment \\n 13. Update Appointment \\n \"\r\n"
-				+ "				+ \"14. Get Appointment \\n 15. Get All Appointment \\n\"");
+		System.out.println(" 1. Add Doctor \n 2. Delete Doctor \n 3. Update Doctor \n "
+				+ "4. Print All Doctor \n 5. Add Patient \n 6. Delete Patient \n 7. Update Patient \n 8. Print all Patient \n"
+				+ " 9. Add Appointment \n 10. Delete Appointment \n 11. Update Appointment \n"
+				+ " 12. Print All Appointment");
 		
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Select Your Option");
 		int option = sc.nextInt();
 		return option;
+	}
+	public void printAllDoctors(List doctorList) {
+		for(int i = 0; i<doctorList.size(); i++) { 
+			System.out.println(doctorList.get(i));
+		}
+	}
+	public void printAllPatients(List patient) {
+		for(int i = 0; i < patient.size(); i++) {
+			System.out.println(patient.get(i));
+			}
+	}
+	public void printAllAppointments(List appointment) {
+		for(int i = 0; i < appointment.size(); i++ ) {
+			System.out.println(appointment.get(i));
+		}
 	}
 }
