a185341f79b98f5dba4035ed82ab3256d8067940
DigvijayKadam1998 <95114271+DigvijayKadam1998@users.noreply.github.com>
2021-12-16 23:31:27 +0530

    Update Appointment.java

--- src/com/bl/hms/Appointment.java
+++ src/com/bl/hms/Appointment.java
@@ -1,11 +1,24 @@
 package com.bl.hms;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+
 
 public class Appointment {
 	
 	String patientId;
-	String doctorId;
+	String appointmentId;
 	int roomNumber;
-	Date appointmentDate;
+	String appointmentDate;
+	
+	 @Override
+	    public String toString() {
+	        return "Appointment{" +
+	                "patientId='" + patientId + '\n' +
+	                ",appointmentId='" + appointmentId + '\n' +
+	                ", roomNumber=" + roomNumber + '\n' +
+	                ",Date=" + appointmentDate +
+	                '}';
+	    }
 }
