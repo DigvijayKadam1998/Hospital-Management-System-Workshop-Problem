5e07fbe05d97bcd8d2113cd1b2cddcd740b30b51
DigvijayKadam1998 <95114271+DigvijayKadam1998@users.noreply.github.com>
2021-12-28 23:22:05 +0530

    Update DoctorRepo.java

--- src/com/bl/hms/DoctorRepo.java
+++ src/com/bl/hms/DoctorRepo.java
@@ -4,10 +4,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DoctorRepo {
-	List <Doctor> doctorList = new ArrayList();
+	private static DoctorRepo instance;
+	List <Doctor> doctorList = new ArrayList<>();
+	
+	private DoctorRepo() {
+		
+	}
+	
+	public static DoctorRepo getInstance() {
+		if(instance == null) {
+			instance = new DoctorRepo();
+		}
+		return instance;
+	}
 	
 	public void add(Doctor doctor) {
 		doctorList.add(doctor);
+	}
+	
+	public void remove(Doctor doctor) {
+		doctorList.remove(doctor);
+	}
+	
+	public Doctor get(String id) {
+		for(int i = 0; i < doctorList.size(); i++) {
+			if(doctorList.get(i).id.equals(id)) {
+				return doctorList.get(i);
+			}
+		}
+		return null;
 	}
 	
 	List getDoctorsList() {
