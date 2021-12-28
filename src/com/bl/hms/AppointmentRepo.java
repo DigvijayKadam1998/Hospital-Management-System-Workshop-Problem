f16068353ea36cb101d3179daf5a2c316c1f06c0
DigvijayKadam1998 <95114271+DigvijayKadam1998@users.noreply.github.com>
2021-12-28 23:22:46 +0530

    Update AppointmentRepo.java

--- src/com/bl/hms/AppointmentRepo.java
+++ src/com/bl/hms/AppointmentRepo.java
@@ -4,7 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AppointmentRepo {
+	private static AppointmentRepo instance;
 	List appointmentList = new ArrayList();
+	
+	private AppointmentRepo() {
+		
+	}
+	
+	public static AppointmentRepo getInstance() {
+		if(instance == null) {
+			instance = new AppointmentRepo();
+		}
+		return instance;
+	}
 	
 	public void add(Appointment appointment) {
 		appointmentList.add(appointment);
@@ -14,3 +26,4 @@ public class AppointmentRepo {
 		return appointmentList;
 	}
 }
+
